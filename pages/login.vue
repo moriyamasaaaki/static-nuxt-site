7990d2767d4b0d2465f461f76c038ef9ca0dad2b
moriyamasaaaki <moriya-7071@outlook.com>
2020-05-15 21:00:09 +0900

    feat(login): error検出とボタンdisabled

--- pages/login.vue
+++ pages/login.vue
@@ -13,26 +13,38 @@
               <div class="field">
                 <div class="control">
                   <input
+                    v-model="form.email"
                     class="input is-large"
                     type="email"
                     placeholder="メールアドレス"
                     autofocus
                     autocomplete="email"
+                    @blur="$v.form.email.$touch()"
                   />
+                  <div v-if="$v.form.email.$error" class="form-error">
+                    <span v-if="!$v.form.email.required" class="help is-danger">Email is required</span>
+                    <span v-if="!$v.form.email.emailValidator" class="help is-danger">Email address is not valid</span>
+                  </div>
                 </div>
               </div>
               <div class="field">
                 <div class="control">
                   <input
+                    v-model="form.password"
                     class="input is-large"
-                    type="password"
                     placeholder="パスワード"
+                    type="password"
                     autocomplete="current-password"
+                    @blur="$v.form.email.$touch()"
                   />
+                  <div v-if="$v.form.password.$error" class="form-error">
+                    <span v-if="!$v.form.password.required" class="help is-danger">Password is required</span>
+                  </div>
                 </div>
               </div>
               <button
-                @click.prevent="() => {}"
+                @click.prevent="login"
+                :disabled="$v.form.$invalid"
                 class="button is-block is-info is-large is-fullwidth"
               >ログイン</button>
             </form>
@@ -42,6 +54,36 @@
     </div>
   </section>
 </template>
+
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+export default {
+  data () {
+    return {
+      form: {
+        email: null,
+        password: null
+      }
+    }
+  },
+  validations: {
+    form: {
+      email: {
+        emailValidator: email,
+        required
+      },
+      password: {
+        required
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$v.form.$touch()
+    }
+  }
+}
+</script>
 
 <style scoped>
 .hero.is-success {
